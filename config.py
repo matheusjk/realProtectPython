8fa84a9856cc95f9f6c37c4cb698cf6e4e61a8ac
matheusjk <matheusrodriguesh1@gmail.com>
2021-04-08 16:32:15 -0300

    venv onde eh criada a maquina virtual python | config.py onde se tem as configuracoes basicas | realProject.sql banco | auth.log arquivo de carga pro banco

---
+++ config.py
@@ -0,0 +1,13 @@
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://teste:123@mudar@localhost:3306/realProtect"
+
+SQLALCHEMY_TRACK_MODIFICATIONS = True
+SECRET_KEY = 'python+iot+linux'
+
+MAIL_SERVER = ''
+MAIL_PORT = 587
+MAIL_USERNAME = ''
+MAIL_PASSWORD = ''
+MAIL_USER_TLS = True
+MAIL_USER_SSL = False
